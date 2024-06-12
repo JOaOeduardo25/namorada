dc6d0f18af61963214c1999113b7283d79d57fd5
Joao Eduardo <joaoeldereduardo@gmail.com>
2024-06-12 17:24:36 -0300

    Atualização do código para melhorar funcionalidades de slideshow e áudio

--- scripts.js
+++ scripts.js
@@ -1,42 +1,45 @@
-let slideIndex = 0;
+let audio = document.getElementById('background-audio');
+let playButton = document.getElementById('play-button');
 
+// Inicialmente, configure o áudio para começar silencioso
+audio.volume = 0;
+
+// Adicione um ouvinte de evento para o botão de reprodução
+playButton.addEventListener('click', function() {
+    // Tente reproduzir o áudio quando o usuário clicar
+    audio.play().then(function() {
+        // Se a reprodução for bem-sucedida, aumente o volume após 1 segundo
+        setTimeout(function() {
+            audio.volume = 1;
+        }, 1000);
+    }).catch(function(error) {
+        // Se ocorrer um erro ao reproduzir, imprima o erro no console
+        console.log('Autoplay failed:', error);
+    });
+});
+
+// Função para iniciar o slideshow, colocando-a fora do evento window.onload
 function showSlides() {
     let slides = document.getElementsByClassName("slide");
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
-    slideIndex++;
-    if (slideIndex > slides.length) {
-        slideIndex = 1;
-    }
-    slides[slideIndex - 1].style.display = "block";
-    setTimeout(showSlides, 3000); // Muda a imagem a cada 3 segundos
-}
+    let slideIndex = 0;
 
-window.onload = function() {
-    showSlides();
-
-    let audio = document.getElementById('background-audio');
-    if (audio) {
-        audio.volume = 0;
-        audio.play().then(() => {
-            setTimeout(() => {
-                audio.volume = 1;
-            }, 1000); // Define um atraso de 1 segundo para aumentar o volume
-        }).catch(error => {
-            console.log('Autoplay failed:', error);
-        });
-    } else {
-        console.log('Elemento de áudio não encontrado.');
-    }
-
-    // Adiciona o ouvinte de evento ao botão de reprodução
-    document.getElementById('play-button').addEventListener('click', function() {
-        var audio = document.getElementById('background-audio');
-        if (audio) {
-            audio.play();
-        } else {
-            console.log('Elemento de áudio não encontrado.');
+    // Função anônima para alterar os slides
+    (function changeSlide() {
+        for (let i = 0; i < slides.length; i++) {
+            slides[i].style.display = "none";
         }
-    });
+        slideIndex++;
+        if (slideIndex > slides.length) {
+            slideIndex = 1;
+        }
+        slides[slideIndex - 1].style.display = "block";
+        setTimeout(changeSlide, 3000); // Muda o slide a cada 3 segundos
+    })();
 }
+
+// Chame a função do slideshow após garantir que o áudio foi encontrado
+audio.addEventListener('canplaythrough', function() {
+    showSlides();
+});
+
+
